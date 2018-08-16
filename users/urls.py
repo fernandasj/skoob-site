1c553426cc6c176d5a068146cb16921408f4fc1f
Fernanda Vieira <fernanda.silva_sj@hotmail.com>
2018-08-16 16:43:09 -0300

    Cadastro usuario

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path, include
-from django.views.generic.base import TemplateView
-
 from .views import registerUser
-
 
 app_name = 'users'
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', registerUser, name='registerUser'),
 ]
