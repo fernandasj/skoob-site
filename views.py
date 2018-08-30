167803ac61670670dc6e413711a96ff3bc15f092
lucasgf07 <lucasgarrido1@hotmail.com>
2018-08-30 15:54:54 -0300

    Criando nova branch

---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+
+def search(request):
+
+    if request.POST:
+	print request.POST['term']
+	return HttpResponseRedirect("/")
+    else:
+	return render_to_response('home.html')
