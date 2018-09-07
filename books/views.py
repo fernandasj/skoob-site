cbc19fcb50b3069e02da934f31f54044f5bc5416
rodgeraraujo <rogerio.araujo@mail.com>
2018-09-07 03:14:49 -0300

    Add mensagem 'Livro nao cadastrando', ao utilizar o motor de busca. fix #8

--- books/views.py
+++ books/views.py
@@ -9,7 +9,12 @@ def Search(request):
         query = request.POST['search']
         result1 = 'Sua busca:';
         result2 = ', retornou os seguintes resultados.';
+        result3 = 'Esse livro não está cadastrado ;(';
+        
         livros = Livro.objects.filter(titulo__contains=query)
-        return render(request, 'results.html', {'livros':livros, 'query':query, 'result1':result1, 'result2':result2})
+        if livros.exists():
+            return render(request, 'results.html', {'livros':livros, 'query':query, 'result1':result1, 'result2':result2})
+        else:
+            return render(request, 'results.html', {'query':query, 'result1':result1, 'result2':result2, 'result3':result3})
     except KeyError:
         return render(request, 'home.html')
